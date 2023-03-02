7ea11f5f38825a0033f6aa45e336f5f445a25dd2
kobemelo <jgalarz95@gmail.com>
2023-03-02 20:21:07 +0000

    filter records committed

--- src/main/java/FilterRecords.java
+++ src/main/java/FilterRecords.java
@@ -10,7 +10,8 @@ import java.util.List;
 /**
  * SQL sublanguage: DQL (Data Query Language)
  *
- * Now that we know how to query all records from a table utilizing the "SELECT" keyword, it might be beneficial to filter what records are given to us from a table.
+ * Now that we know how to query all records from a table utilizing the "SELECT" keyword, it might be beneficial to 
+ * filter what records are given to us from a table.
  *
  * For example lets look at the "employee" table below:
  *      employee table
@@ -27,7 +28,8 @@ import java.util.List;
  * The statement that will be utilized is as follows:
  * SELECT * FROM employee WHERE first_name = 'Steve';
  *
- * In the above statement, the "WHERE" keyword allows us to filter the records based on the condition defined after the keyword.
+ * In the above statement, the "WHERE" keyword allows us to filter the records based on the condition
+ *  defined after the keyword.
  * The condition that we specified is that the first_name column must have the value "Steve".
  *
  * This statement would have returned the following result set:
@@ -37,7 +39,8 @@ import java.util.List;
  *      |3     |'Steve'         |'Jones'        |99890.99  |
  *
  * In addition to filtering on equality like above, we can filter on inequality with the <, >, <=, >=, and != operators.
- * We can even filter on strings that match partially, using the LIKE keyword and the '%' wildcard. Here are a few examples:
+ * We can even filter on strings that match partially, using the LIKE keyword and the '%' wildcard. 
+ * Here are a few examples:
  * This will return all records where the first name starts with 'S'
  *      SELECT * FROM employees WHERE first_name LIKE 'S%'
  * This will return all the records with a salary value of less than $100,000.00
@@ -88,7 +91,7 @@ public class FilterRecords {
         return users;
     }
     /**
-     * Problem 3: Given the employee table, write a query in the problem2.sql file to retrieve all the records
+     * Problem 2: Given the employee table, write a query in the problem2.sql file to retrieve all the records
      * from the employee table that have a salary greater than $75000
      *
      *   employee table
